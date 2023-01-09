c314b9a8c7c716d58e0c73caaa1e8eef29c1d4d2
sussa3007 <sussa1933@gmail.com>
2023-01-10 00:23:15 +0900

    feat: WebHook Service & Dto 작성

---
+++ server/src/main/java/com/mainproject/server/webhook/WebHookDto.java
@@ -0,0 +1,14 @@
+package com.mainproject.server.webhook;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WebHookDto {
+
+    private String content;
+
+}
