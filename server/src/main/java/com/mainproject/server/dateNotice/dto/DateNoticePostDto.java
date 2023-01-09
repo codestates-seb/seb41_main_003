3a372a4d4458f208919b9d3af53ee888174f5df9
HOSOO KANG <hosoo3513@gmail.com>
2023-01-09 15:46:00 +0900

    feat: DateNotice Part Dto 작성

---
+++ server/src/main/java/com/mainproject/server/dateNotice/dto/DateNoticePostDto.java
@@ -0,0 +1,15 @@
+package com.mainproject.server.dateNotice.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DateNoticePostDto {
+    private Long dateNoticeId;
+    private String scheduleTitle;
+    private String scheduleBody;
+    private String noticeTitle;
+    private String noticeBody;
+    private String HomeworkBody;
+}
