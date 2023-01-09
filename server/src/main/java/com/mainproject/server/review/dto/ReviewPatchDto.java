f5b3c8ff75fddc26231827db13096f668a66c302
HOSOO KANG <hosoo3513@gmail.com>
2023-01-09 15:45:44 +0900

    feat: Review Part Dto 작성

--- server/src/main/java/com/mainproject/server/review/dto/ReviewPatchDto.java
+++ server/src/main/java/com/mainproject/server/review/dto/ReviewPatchDto.java
@@ -3,21 +3,13 @@ package com.mainproject.server.review.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
 @Getter
 @Setter
-public class ReviewResponseDto {
+public class ReviewPatchDto {
     private Long reviewId;
     private int professional;
     private int readiness;
     private int explanation;
     private int punctuality;
     private String reviewBody;
-    // 프로필 이름
-    private String name;
-    private String tutoringId;
-    private LocalDateTime createAt;
-    private LocalDateTime updateAt;
-
 }
