8eecbeae02b1976670bc4a8567864df55db6d0c2
강호수 <hosoo3513@gmail.com>
2023-01-09 22:39:30 +0900

    feat: tutoring controller 작성

--- server/src/main/java/com/mainproject/server/tutoring/controller/TutoringController.java
+++ server/src/main/java/com/mainproject/server/tutoring/controller/TutoringController.java
@@ -1,4 +1,84 @@
 package com.mainproject.server.tutoring.controller;
 
+import com.mainproject.server.dto.PageResponseDto;
+import com.mainproject.server.dto.ResponseDto;
+import com.mainproject.server.tutoring.dto.TutoringSimpleResponseDto;
+import com.mainproject.server.utils.StubData;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/tutoring")
 public class TutoringController {
+
+    private StubData stubData;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity getAllTutoringByUser(@PathVariable String userId) {
+        return new ResponseEntity(ResponseDto.of(stubData.createTutoringSimpleResponse()), HttpStatus.OK);
+    }
+
+    @PostMapping("/{userId}")
+    public ResponseEntity postTutoring(@PathVariable String userId) {
+        return new ResponseEntity(ResponseDto.of(stubData.createTutoringSimpleResponse()), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/profiles/{profileId}")
+    public ResponseEntity getAllTutoring(@PathVariable String profileId,
+                                         @PageableDefault(page = 0, size = 10, sort = "profileId", direction = Sort.Direction.DESC)
+                                         Pageable pageable) {
+        TutoringSimpleResponseDto tutoringSimpleResponse = stubData.createTutoringSimpleResponse();
+        List<TutoringSimpleResponseDto> tutoringList = List.of(tutoringSimpleResponse, tutoringSimpleResponse, tutoringSimpleResponse);
+        Page<TutoringSimpleResponseDto> page = new PageImpl<>(tutoringList, pageable, 10L);
+        PageResponseDto response = PageResponseDto.of(tutoringList, page);
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/details/{tutoringId}")
+    public ResponseEntity getTutoring(@PathVariable String tutoringId) {
+        return new ResponseEntity(ResponseDto.of(stubData.createTutoringResponse()), HttpStatus.OK);
+    }
+
+    @PatchMapping("/details/{tutoringId}")
+    public ResponseEntity patchTutoring(@PathVariable String tutoringId) {
+        return new ResponseEntity(ResponseDto.of(stubData.createTutoringResponse()), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/details/{tutoringId}")
+    public ResponseEntity deleteTutoring(@PathVariable String tutoringId) {
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/date-notice")
+    public ResponseEntity postDateNotice() {
+        return new ResponseEntity(ResponseDto.of(stubData.createDateNoticeResponse()), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/date-notice/{dateNoticeId}")
+    public ResponseEntity getDateNotice(@PathVariable String dateNoticeId) {
+        return new ResponseEntity(ResponseDto.of(stubData.createDateNoticeResponse()), HttpStatus.OK);
+    }
+
+    @PatchMapping("/date-notice/{dateNoticeId}")
+    public ResponseEntity patchDateNotice(@PathVariable String dateNoticeId) {
+        return new ResponseEntity(ResponseDto.of(stubData.createDateNoticeResponse()), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/date-notice/{dateNoticeId}")
+    public ResponseEntity deleteDateNotice(@PathVariable String dateNoticeId) {
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
 }
