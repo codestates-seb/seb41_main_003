5ffa4d78af777aac196b27fa6d02c932cbe4c0eb
Seung gu Shin <less0805@gmail.com>
2023-01-10 17:37:31 +0900

    feat: Confirm Modal 추가

--- client/src/pages/AddProfile.jsx
+++ client/src/pages/AddProfile.jsx
@@ -2,6 +2,7 @@ import ChangeProfileCard from '../components/ChangeProfile/ChangeProfileCard';
 import ChangeProfileContents from '../components/ChangeProfile/ChangeProfileContents';
 import styles from './ChangeProfile.module.css';
 import { useState } from 'react';
+import { ConfirmModal } from '../components/Modal';
 
 const initialState = {
   profile_id: 0,
@@ -24,12 +25,33 @@ const initialState = {
 
 const AddProfile = () => {
   const [user, setUser] = useState(initialState);
+  const [confirm, setConfirm] = useState(false);
+  const confirmText = `입력한 내용으로
+  프로필 작성을 완료하시겠습니까?`;
+
+  const confirmHandler = (e) => {
+    if (e.target.name === 'yes') {
+      console.log('추가 요청'); // TODO : POST 요청
+      setConfirm(!confirm); // TODO : 리다이렉트 함수
+    } else {
+      setConfirm(!confirm);
+    }
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <ChangeProfileCard user={user} setUser={setUser} isNew={true} />
+        <ChangeProfileCard
+          user={user}
+          setUser={setUser}
+          isNew={true}
+          setConfirm={setConfirm}
+        />
         <ChangeProfileContents user={user} setUser={setUser} />
       </div>
+      {confirm ? (
+        <ConfirmModal text={confirmText} modalHandler={confirmHandler} />
+      ) : undefined}
     </div>
   );
 };
