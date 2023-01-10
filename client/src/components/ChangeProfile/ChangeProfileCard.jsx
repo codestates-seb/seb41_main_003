60eb4da78bd1d716798b366f9b53f6b29dfcdd4e
Seung gu Shin <less0805@gmail.com>
2023-01-10 17:03:11 +0900

    fix: subjectHandler subjectId 오류 해결

--- client/src/components/ChangeProfile/ChangeProfileCard.jsx
+++ client/src/components/ChangeProfile/ChangeProfileCard.jsx
@@ -2,20 +2,39 @@ import { LabelTextInput } from '../Input';
 import { ButtonNightBlue } from '../Button';
 import styles from './ChangeProfileCard.module.css';
 import { MdMode } from 'react-icons/md';
-import { useState } from 'react';
 import PropType from 'prop-types';
 
-const ChangeProfileCard = ({ isNew = true }) => {
-  const [subject, setSubject] = useState([]);
+const ChangeProfileCard = ({ isNew = true, user, setUser, setConfirm }) => {
+  const { name, bio, school, subjects, profile_status } = user;
+
+  const subjectTitles = subjects.map((obj) => obj.subjectTitle);
+
+  const inputHandler = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
 
   const subjectHandler = (e) => {
     const { name } = e.target;
-    if (subject.includes(name)) {
-      setSubject(subject.filter((el) => el !== name));
+    const id = ['_', '영어', '수학', '국어', '사회', '과학', '자격증', '기타'];
+    if (subjectTitles.includes(name)) {
+      setUser({
+        ...user,
+        subjects: user.subjects.filter((obj) => obj.subjectTitle !== name),
+      });
     } else {
-      setSubject([...subject, name]);
+      setUser({
+        ...user,
+        subjects: [
+          ...user.subjects,
+          {
+            subjectId: id.indexOf(name),
+            subjectTitle: name,
+            content: '',
+          },
+        ],
+      });
     }
-    console.log(subject);
   };
   return (
     <div className={styles.container}>
@@ -31,84 +50,95 @@ const ChangeProfileCard = ({ isNew = true }) => {
           name="이름"
           placeHolder="이름"
           type="text"
-          value=""
+          value={name}
+          handler={inputHandler}
         />
         <LabelTextInput
           id="bio"
           name="한 줄 소개"
           placeHolder="한 줄 소개"
           type="text"
-          value=""
+          value={bio}
+          handler={inputHandler}
         />
         <LabelTextInput
           id="school"
-          name="학교 / 학번"
-          placeHolder="학교 / 학번"
+          name={profile_status === 'TUTOR' ? '학교 / 학번' : '학년'}
+          placeHolder={profile_status === 'TUTEE' ? '학교 / 학번' : '학년'}
           type="text"
-          value=""
+          value={school}
+          handler={inputHandler}
         />
       </div>
       <div className={styles.subject}>
         과목
         <div className={styles.btnContain}>
           <button
-            name="en"
-            className={subject.includes('en') ? styles.active : ''}
+            name="영어"
+            className={subjectTitles.includes('영어') ? styles.active : ''}
             onClick={subjectHandler}
           >
             영어
           </button>
           <button
-            name="ma"
-            className={subject.includes('ma') ? styles.active : ''}
+            name="수학"
+            className={subjectTitles.includes('수학') ? styles.active : ''}
             onClick={subjectHandler}
           >
             수학
           </button>
           <button
-            name="ko"
-            className={subject.includes('ko') ? styles.active : ''}
+            name="국어"
+            className={subjectTitles.includes('국어') ? styles.active : ''}
             onClick={subjectHandler}
           >
             국어
           </button>
           <button
-            name="so"
-            className={subject.includes('so') ? styles.active : ''}
+            name="사회"
+            className={subjectTitles.includes('사회') ? styles.active : ''}
             onClick={subjectHandler}
           >
             사회
           </button>
           <button
-            name="sc"
-            className={subject.includes('sc') ? styles.active : ''}
+            name="과학"
+            className={subjectTitles.includes('과학') ? styles.active : ''}
             onClick={subjectHandler}
           >
             과학
           </button>
           <button
-            name="ce"
-            className={subject.includes('ce') ? styles.active : ''}
+            name="자격증"
+            className={subjectTitles.includes('자격증') ? styles.active : ''}
             onClick={subjectHandler}
           >
             자격증
           </button>
           <button
-            name="etc"
-            className={subject.includes('etc') ? styles.active : ''}
+            name="기타"
+            className={subjectTitles.includes('기타') ? styles.active : ''}
             onClick={subjectHandler}
           >
             기타
           </button>
         </div>
       </div>
-      <ButtonNightBlue text={isNew ? '추가완료' : '수정완료'} />
+      <ButtonNightBlue
+        text={isNew ? '추가완료' : '수정완료'}
+        buttonHandler={() => {
+          setConfirm((prev) => !prev);
+        }}
+      />
     </div>
   );
 };
 
 ChangeProfileCard.propTypes = {
   isNew: PropType.bool,
+  user: PropType.object,
+  setUser: PropType.func,
+  setConfirm: PropType.func,
 };
 
 export default ChangeProfileCard;
