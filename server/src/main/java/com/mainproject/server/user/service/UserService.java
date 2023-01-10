731e711f07d478ca082ad14ae1dc07463d3cf1ec
sussa3007 <sussa1933@gmail.com>
2023-01-11 02:33:01 +0900

    feat: Security UserDetails 적용을 위한 UserService 로직 작성

--- server/src/main/java/com/mainproject/server/user/service/UserService.java
+++ server/src/main/java/com/mainproject/server/user/service/UserService.java
@@ -1,11 +1,17 @@
 package com.mainproject.server.user.service;
 
 
+import com.mainproject.server.constant.ErrorCode;
+import com.mainproject.server.constant.LoginType;
+import com.mainproject.server.exception.ServiceLogicException;
+import com.mainproject.server.user.entity.User;
 import com.mainproject.server.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -14,5 +20,27 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public User getUser(Long userId) {
+        return verifiedUserById(userId);
+    }
+
+    public User createUser(User user) {
+        if (user.getLoginType().equals(LoginType.SOCIAL)) {
+            // Todo 소셜 로그인 경우 임의의 password 저장
+        }
+        return userRepository.save(user);
+    }
+
+    public User verifiedUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new ServiceLogicException(ErrorCode.USER_NOT_FOUND));
+
+    }
+
+    public User verifiedUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new ServiceLogicException(ErrorCode.USER_NOT_FOUND));
+    }
 
 }
