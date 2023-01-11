ab46c66587537025b6efedb8b47b3034d8290891
sussa3007 <sussa1933@gmail.com>
2023-01-11 12:06:29 +0900

    feat: User & PatchDto 수정
    - User 첫 가입시 필수가 아닌 필드 수정
    - Patch User 시 필수 필드 벨리데이션 적용

--- server/src/main/java/com/mainproject/server/user/dto/UserPatchDto.java
+++ server/src/main/java/com/mainproject/server/user/dto/UserPatchDto.java
@@ -2,6 +2,8 @@ package com.mainproject.server.user.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
@@ -15,8 +17,13 @@ public class UserPatchDto {
 
     private String password;
 
+    @NotBlank
+    private String phoneNumber;
+
+    @NotBlank
     private String secondPassword;
 
+    @NotBlank
     private String userStatus;
 
 }
