d27fe20a427b435c6aa4042b5834ae01a447b6e0
sussa3007 <sussa1933@gmail.com>
2023-01-12 16:10:52 +0900

    feat: Profile Mapper 구현

--- server/src/main/java/com/mainproject/server/profile/mapper/ProfileMapper.java
+++ server/src/main/java/com/mainproject/server/profile/mapper/ProfileMapper.java
@@ -1,6 +1,7 @@
 package com.mainproject.server.profile.mapper;
 
 
+import com.mainproject.server.image.entity.ProfileImage;
 import com.mainproject.server.profile.dto.ProfileDto;
 import com.mainproject.server.profile.dto.ProfileSimpleResponseDto;
 import com.mainproject.server.profile.entity.Profile;
@@ -15,15 +16,39 @@ public interface ProfileMapper {
         if ( profile == null ) {
             return null;
         }
+        ProfileImage profileImage = profile.getProfileImage();
+        String url = null;
+        if (profileImage != null) {
+            url = profileImage.getUrl();
+        }
         return ProfileSimpleResponseDto.builder()
                 .profileId(profile.getProfileId())
                 .name(profile.getName())
                 .school(profile.getSchool())
-                .url(profile.getProfileImage().getUrl())
+                .url(url)
                 .build();
     }
 
-    Profile dtoToEntity(ProfileDto profileDto);
+    default Profile dtoToEntity(ProfileDto profileDto) {
+        if ( profileDto == null ) {
+            return null;
+        }
+
+        Profile profile = new Profile();
+
+        profile.setName( profileDto.getName() );
+        profile.setBio( profileDto.getBio() );
+        profile.setWantDate( profileDto.getWantDate() );
+        profile.setPay( profileDto.getPay() );
+        profile.setWay( profileDto.getWay() );
+        profile.setGender( profileDto.getGender() );
+        profile.setSchool( profileDto.getSchool() );
+        profile.setCharacter( profileDto.getCharacter() );
+        profile.setPreTutoring( profileDto.getPreTutoring() );
+        profile.setDifference( profileDto.getDifference() );
+
+        return profile;
+    }
 
     List<ProfileSimpleResponseDto> entityListToSimpleResponseDtoList(List<Profile> profiles);
 
