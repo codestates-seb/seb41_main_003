677adc6868c5892e2960f9b85d0582129e9fa901
Seung gu Shin <less0805@gmail.com>
2023-01-13 23:25:04 +0900

    refactor: 컴포넌트 파일 분리 및 코드 리팩토링

--- client/src/components/profileSection/ProfileCard.jsx
+++ client/src/components/profileSection/ProfileCard.jsx
@@ -6,36 +6,33 @@ import defaultUser from '../../assets/defaultUser.png';
 import { MdStar } from 'react-icons/md';
 
 const ProfileCard = ({ user, QuestionModalHandler }) => {
+  const { name, rate, bio, school, subjects } = user;
   return (
     <div className={styles.cardContainer}>
+      {/* //! API 연결 시 유저 이미지 수정 필요 */}
       <img alt="user img" src={defaultUser} />
       <section className={styles.textContainer}>
         <div className={styles.starLine}>
-          <p className={styles.font1}>{user.name}</p>
+          <h3 className={styles.font1}>{name}</h3>
           <div className={styles.starBox}>
             <MdStar fill="#F0C24D" size="21" />
-            <p className={styles.paragragh}>{user.rate} / 5 </p>
+            <p className={styles.paragragh}>{rate} / 5 </p>
           </div>
         </div>
         <div>
           <p className={styles.font4}>한 줄 소개</p>
-          <p className={styles.paragragh}>{user.bio}</p>
+          <p className={styles.paragragh}>{bio}</p>
         </div>
         <div>
           <p className={styles.font4}>학교</p>
-          <p className={styles.paragragh}>{user.school}</p>
+          <p className={styles.paragragh}>{school}</p>
         </div>
         <div>
           <p className={styles.font4}>과목</p>
           <span className={styles.subjects}>
             <ul>
-              {user.subjects.map((subject) => {
-                return (
-                  <BlueSubject
-                    key={subject.subjectId}
-                    text={subject.subjectTitle}
-                  />
-                );
+              {subjects.map(({ subjectId, subjectTitle }) => {
+                return <BlueSubject key={subjectId} text={subjectTitle} />;
               })}
             </ul>
           </span>
