91eec97148cedc1ca9be19b6e1b1b1a5380462bf
Seung gu Shin <less0805@gmail.com>
2023-01-15 17:20:02 +0900

    refactor: Message 모달 리팩토링 #81

--- client/src/components/Message/MessageContent.jsx
+++ client/src/components/Message/MessageContent.jsx
@@ -3,68 +3,66 @@ import { TextInput } from '../Input';
 import { MdMenu } from 'react-icons/md';
 import PropType from 'prop-types';
 import { useState } from 'react';
-import {
-  AlertModal,
-  ConfirmModal,
-  ConfirmTextModal,
-} from '../modal/DefaultModal';
 import Chat from './Chat';
+import { useSetRecoilState, useResetRecoilState } from 'recoil';
+import ModalState from '../../recoil/modal.js';
 
 const MessageContent = ({ messages }) => {
   const [isMenu, setIsMenu] = useState(false);
   const [inputValue, setInputValue] = useState('');
 
-  // * Modal 창 관련 상태, 핸들러
-  // TODO : 모달 컴포넌트 분기 및 상태 정리 필요
-  const [isMatchConfirm, setIsMatchConfirm] = useState({
-    content: `상대방의 요청 수락 시 매칭이 완료됩니다.
+  const setModal = useSetRecoilState(ModalState);
+  const resetModal = useResetRecoilState(ModalState);
+
+  // * Modal 창 관련 props
+  const matchConfirmProps = {
+    isOpen: true,
+    modalType: 'confirmText',
+    props: {
+      text: `상대방의 요청 수락 시 매칭이 완료됩니다.
     매칭 요청 하시겠습니까?
 
     매칭을 원하신다면 과외의 이름을 작성해주세요.
     `,
-    value: '',
-    isOpen: false,
-  });
-  const [isCancelConfirm, setIsCancelConfirm] = useState({
-    content: `상담 취소 하시겠습니까?
-      상담 취소 시 대화 내역이 모두 삭제됩니다.`,
-    isOpen: false,
-  });
-  const [isMatchAlert, setIsMatchAlert] = useState({
-    content: `매칭 요청이 완료되었습니다.
-    상대방의 요청 수락 이후에는 과외 관리 페이지에서
-    확인하실 수 있습니다.`,
-    isOpen: false,
-  });
-  const [isCancelAlert, setIsCancelAlert] = useState({
-    content: `상담이 취소되었습니다.`,
-    isOpen: false,
-  });
-
-  const cancelHandler = (e) => {
-    const { name } = e.target;
-    setIsCancelConfirm({
-      ...isCancelConfirm,
-      isOpen: !isCancelConfirm.isOpen,
-    });
-    if (name === 'yes') {
-      // TODO : 상담 취소 관련 API 연결 필요
-      setIsCancelAlert({ ...isCancelAlert, isOpen: !isCancelAlert.isOpen });
-    }
+      modalHandler: (_, value) => {
+        // TODO : 매칭 신청 관련 API 필요
+        console.log(value);
+        resetModal();
+        setModal(matchAlertProps);
+      },
+      placeHolder: '과외의 이름을 작성하세요',
+    },
   };
 
-  const matchHandler = (e) => {
-    const { name } = e.target;
-    setIsMatchConfirm({
-      ...isMatchConfirm,
-      isOpen: !isMatchConfirm.isOpen,
-      value: '',
-    });
-    if (name === 'yes') {
-      // TODO : 매칭 신청 관련 API 필요
-      console.log(isMatchConfirm.value);
-      setIsMatchAlert({ ...isMatchAlert, isOpen: !isMatchAlert.isOpen });
-    }
+  const cancelConfirmProps = {
+    isOpen: true,
+    modalType: 'confirm',
+    props: {
+      text: `상담 취소 하시겠습니까?
+      상담 취소 시 대화 내역이 모두 삭제됩니다.`,
+      modalHandler: () => {
+        // TODO : 상담 취소 관련 API 필요
+        console.log('상담 취소');
+        resetModal();
+        setModal(cancelAlertProps);
+      },
+    },
+  };
+
+  const matchAlertProps = {
+    isOpen: true,
+    modalType: 'alert',
+    props: {
+      text: `매칭 요청이 완료되었습니다.
+    상대방의 요청 수락 이후에는 과외 관리 페이지에서
+    확인하실 수 있습니다.`,
+    },
+  };
+
+  const cancelAlertProps = {
+    isOpen: true,
+    modalType: 'alert',
+    props: { text: `상담이 취소되었습니다.` },
   };
 
   return (
@@ -96,68 +94,15 @@ const MessageContent = ({ messages }) => {
         {/* dropDown */}
         {isMenu && (
           <div className={styles.dropdown}>
-            <button
-              onClick={() => {
-                setIsMatchConfirm({
-                  ...isMatchConfirm,
-                  isOpen: !isMatchConfirm.isOpen,
-                });
-              }}
-            >
+            <button onClick={() => setModal(matchConfirmProps)}>
               매칭 요청
             </button>
-            <button
-              onClick={() => {
-                setIsCancelConfirm({
-                  ...isCancelConfirm,
-                  isOpen: !isCancelConfirm.isOpen,
-                });
-              }}
-            >
+            <button onClick={() => setModal(cancelConfirmProps)}>
               상담 취소
             </button>
           </div>
         )}
       </div>
-
-      {/* // * 모달 */}
-      {/* // TODO : 모달 컴포넌트 분기 및 상태 정리 필요 */}
-      {isMatchConfirm.isOpen && (
-        <ConfirmTextModal
-          text={isMatchConfirm.content}
-          modalHandler={matchHandler}
-          value={isMatchConfirm.value}
-          valueHandler={(e) => {
-            setIsMatchConfirm({ ...isMatchConfirm, value: e.target.value });
-          }}
-          placeHolder="과외의 이름을 작성해주세요"
-        />
-      )}
-      {isCancelConfirm.isOpen && (
-        <ConfirmModal
-          text={isCancelConfirm.content}
-          modalHandler={cancelHandler}
-        />
-      )}
-      {isMatchAlert.isOpen && (
-        <AlertModal
-          text={isMatchAlert.content}
-          modalHandler={() =>
-            setIsMatchAlert({ ...isMatchAlert, isOpen: !isMatchAlert.isOpen })
-          }
-        />
-      )}
-      {isCancelAlert.isOpen && (
-        <AlertModal
-          text={isCancelAlert.content}
-          modalHandler={() =>
-            setIsCancelAlert({
-              ...isCancelAlert,
-              isOpen: !isCancelAlert.isOpen,
-            })
-          }
-        />
-      )}
     </div>
   );
 };
