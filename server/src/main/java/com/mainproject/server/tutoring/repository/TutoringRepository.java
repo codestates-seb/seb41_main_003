519152fad759d7e49da275916abff5452af9aeee
sussa3007 <sussa1933@gmail.com>
2023-01-15 17:48:58 +0900

    refactor: Tutoring 검색시 TutoringStatus에 따라 검색가능 하도록 Repository 메소드 수정 및 추가

--- server/src/main/java/com/mainproject/server/tutoring/repository/TutoringRepository.java
+++ server/src/main/java/com/mainproject/server/tutoring/repository/TutoringRepository.java
@@ -1,10 +1,21 @@
 package com.mainproject.server.tutoring.repository;
 
+import com.mainproject.server.constant.TutoringStatus;
 import com.mainproject.server.tutoring.entity.Tutoring;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TutoringRepository extends JpaRepository<Tutoring, Long> {
-    Page<Tutoring> findAllByTutorProfileIdOrTuteeProfileId(Long tutorId, Long tuteeId, Pageable pageable);
+    Page<Tutoring> findAllByTutoringStatusAndTutorProfileId(
+            TutoringStatus tutoringStatus,
+            Long tutorId,
+            Pageable pageable
+    );
+
+    Page<Tutoring> findAllByTutoringStatusAndTuteeProfileId(
+            TutoringStatus tutoringStatus,
+            Long tuteeId,
+            Pageable pageable
+    );
 }
