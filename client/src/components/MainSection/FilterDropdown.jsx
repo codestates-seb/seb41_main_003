be99ce4f14045118825fafb97ddc4be1536d2a02
ansmeer008 <ansmeer008@gmail.com>
2023-01-16 15:58:29 +0900

    feat: TutorList 컴포넌트 get 요청시 필요한 파라미터 변수들 세팅 #92

--- client/src/components/MainSection/FilterDropdown.jsx
+++ client/src/components/MainSection/FilterDropdown.jsx
@@ -1,12 +1,29 @@
 import styles from './FilterDropdown.module.css';
+import PropType from 'prop-types';
 
-const FilterDropdown = () => {
+const FilterDropdown = ({ setFilter }) => {
+  const clickHandler = (e) => {
+    setFilter(e.target.name);
+  };
+
   return (
     <ul className={styles.dropdownContainer}>
-      <li className={styles.dropdown}>최신 순</li>
-      <li className={styles.dropdown}>평점 순</li>
+      <li className={styles.dropdown}>
+        <button name="createAt" onClick={clickHandler}>
+          최신 순
+        </button>
+      </li>
+      <li className={styles.dropdown}>
+        <button name="rate" onClick={clickHandler}>
+          평점 순
+        </button>
+      </li>
     </ul>
   );
+};
+
+FilterDropdown.propTypes = {
+  setFilter: PropType.func,
 };
 
 export default FilterDropdown;
--- client/src/pages/TutorList.jsx
+++ client/src/pages/TutorList.jsx
@@ -148,23 +148,36 @@ const TutorListData = [
   },
 ];
 
-//ToDo: 필터 버튼이 눌릴 때마다 (onClick value 등이 바뀔 때마다) - 아마 useEffect 사용할듯
-//tutorData 새롭게 갱신될 수 있도록 해당 필터 API 데이터 통신과 연결 필요함
+//TODO: 튜터 리스트를 불러오는 GET 요청 필요
+//정렬 필터 파라미터 : sort = rate (기본값이 createAt 이므로 별점 순으로 변경시에만 파라미터 붙여서 요청하면 됨)
+//과목 버튼 필터 파라미터 : subjectMenu 상태에서 꺼내와서 subject = 수학,영어,과학 같은 형식으로 요청
+//검색창에서 검색 시 name = 강호수 와 같이 요청함
 
 const TutorList = () => {
+  //정렬 메뉴 오픈 상태
   const [isOpen, setIsOpen] = useState(false);
-  const [isClicked, setIsClicked] = useState([]);
+  //과목 필터 메뉴에서 선택한 과목들
+  const [subjectMenu, setSubjectMenu] = useState([]);
+  //검색창에서 입력한 검색어
+  const [search, setSearch] = useState('');
+  //정렬 메뉴 '최신 순'인지 '별점 순'인지
+  const [filter, setFilter] = useState('');
 
+  //정렬 메뉴 오픈 핸들러
   const filterHandler = () => {
     setIsOpen(!isOpen);
   };
 
-  const clickHandler = (e) => {
-    if (isClicked.includes(e.target.value)) {
-      setIsClicked(isClicked.filter((el) => el !== e.target.value));
+  const subjectHandler = (e) => {
+    if (subjectMenu.includes(e.target.value)) {
+      setSubjectMenu(subjectMenu.filter((el) => el !== e.target.value));
     } else {
-      setIsClicked([e.target.value, ...isClicked]);
+      setSubjectMenu([e.target.value, ...subjectMenu]);
     }
+  };
+
+  const searchHandler = (e) => {
+    setSearch(e.target.value);
   };
 
   return (
@@ -179,20 +192,28 @@ const TutorList = () => {
               <div className={styles.iconBox}>
                 <MdSearch className={styles.mdSearch} />
               </div>
-              <input className={styles.input} placeholder="서울대학교"></input>
+              <input
+                className={styles.input}
+                placeholder="서울대학교"
+                value={search}
+                onChange={searchHandler}
+              ></input>
             </div>
           </div>
         </div>
         <div className={styles.menuContainer}>
-          <MenuButtons isClicked={isClicked} clickHandler={clickHandler} />
+          <MenuButtons
+            subjectMenu={subjectMenu}
+            subjectHandler={subjectHandler}
+          />
           <div
             className={styles.filter}
             onClick={filterHandler}
             aria-hidden="true"
           >
             <MdFilterList className={styles.mdFilterList} />
-            <span>최신 순</span>
-            {isOpen ? <FilterDropdown /> : null}
+            <span>{filter === 'rate' ? '평점 순' : '최신 순'}</span>
+            {isOpen ? <FilterDropdown setFilter={setFilter} /> : null}
           </div>
         </div>
         <div className={styles.feedContainer}>
