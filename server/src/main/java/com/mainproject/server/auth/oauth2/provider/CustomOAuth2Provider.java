b2a5e70b3ce2dbc1631afb63945f5068ba467883
sussa3007 <sussa1933@gmail.com>
2023-01-17 14:02:13 +0900

    CI/CD Update

--- server/src/main/java/com/mainproject/server/auth/oauth2/provider/CustomOAuth2Provider.java
+++ server/src/main/java/com/mainproject/server/auth/oauth2/provider/CustomOAuth2Provider.java
@@ -40,7 +40,8 @@ public enum CustomOAuth2Provider {
         }
     };
 
-    private static final String DEFAULT_LOGIN_REDIRECT_URL = "{baseUrl}/login/oauth2/code/{registrationId}";
+    private static final String DEFAULT_LOGIN_REDIRECT_URL =
+            "http://ec2-15-165-186-53.ap-northeast-2.compute.amazonaws.com/login/oauth2/code/{registrationId}";
 
     protected final ClientRegistration.Builder getBuilder(
             String registrationId, ClientAuthenticationMethod method, String redirectUri) {
