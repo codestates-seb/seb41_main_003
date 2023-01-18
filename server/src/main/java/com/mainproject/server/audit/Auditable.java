b7af57301e87ecacd1d884bc44aa6d28b2c84457
sussa3007 <sussa1933@gmail.com>
2023-01-18 17:40:03 +0900

    refactor: 타임 스템프 Setter 추가

--- server/src/main/java/com/mainproject/server/audit/Auditable.java
+++ server/src/main/java/com/mainproject/server/audit/Auditable.java
@@ -1,6 +1,7 @@
 package com.mainproject.server.audit;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -9,9 +10,9 @@ import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 
 @Getter
+@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class Auditable {
