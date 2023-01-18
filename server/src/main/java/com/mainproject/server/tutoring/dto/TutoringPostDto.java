49f05cbd84f36ee22cca091ee21e4217e04c748f
sussa3007 <sussa1933@gmail.com>
2023-01-18 15:12:30 +0900

    chore: TutoringPostDto ToString 추가

--- server/src/main/java/com/mainproject/server/tutoring/dto/TutoringPostDto.java
+++ server/src/main/java/com/mainproject/server/tutoring/dto/TutoringPostDto.java
@@ -2,12 +2,14 @@ package com.mainproject.server.tutoring.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
+@ToString
 public class TutoringPostDto {
 
     @NotNull
