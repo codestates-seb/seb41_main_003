2bf8ddfcc320027f71a3a974496fd394cb820fcd
Seung gu Shin <less0805@gmail.com>
2023-01-18 12:08:04 +0900

    refactor: 불필요 import 및 주석 제거

--- client/src/components/modal/AdminModal.jsx
+++ client/src/components/modal/AdminModal.jsx
@@ -1,43 +1,10 @@
 import { useEffect, useState } from 'react';
 import styles from './AdminModal.module.css';
-import defaultUser from '../../assets/defaultUser.png';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 import reIssueToken from '../../util/reIssueToken';
 import { useSetRecoilState, useResetRecoilState } from 'recoil';
 import Profile from '../../recoil/profile';
 import ModalState from '../../recoil/modal';
-
-const initialState = {
-  data: [
-    {
-      profileId: 1,
-      name: '신승구',
-      url: 'http://localhost:3000',
-    },
-    {
-      profileId: 2,
-      name: '김민경',
-      url: 'http://localhost:3000',
-    },
-    {
-      profileId: 3,
-      name: '유영민',
-      url: 'http://localhost:3000',
-    },
-    {
-      profileId: 4,
-      name: '김다은',
-      url: 'http://localhost:3000',
-    },
-  ],
-  pageInfo: {
-    page: 1,
-    size: 10,
-    totalElements: 10,
-    totalPages: 1,
-  },
-};
 
 const AdminModal = () => {
   // TODO : 프로필 전환 API 연결 필요
@@ -54,10 +21,6 @@ const AdminModal = () => {
     console.log(name.slice(7));
     setProfile((prev) => ({ ...prev, profileId: name.slice(7) }));
     window.location.href = '/';
-    // TODO : 프로필 전환 로직 추가 필요
-    //특정 프로필 조회 요청을 통해 switchHandler로 받은 profileId를 파라미터로 붙여 아래와 같이 요청 보낸다
-    // GET 요청 : /profiles/details/{profileId}
-    //응답으로 받은 profileId로 로컬이나 전역 상태에 저장되어 있는 프로필 아이디로 업데이트 해준다.
   };
 
   const getUserProfile = async () => {
