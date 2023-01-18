2c32007ba8acb0c9aae434ee61e195ec5f639d7d
sussa3007 <sussa1933@gmail.com>
2023-01-18 16:07:06 +0900

    chore: 불필요한 CORS 헤더 제거

--- server/src/main/java/com/mainproject/server/config/SecurityConfig.java
+++ server/src/main/java/com/mainproject/server/config/SecurityConfig.java
@@ -142,11 +142,6 @@ public class SecurityConfig {
         corsConfiguration.setMaxAge(493772L);
         corsConfiguration.addAllowedOrigin("http://localhost:3000");
         corsConfiguration.addAllowedOrigin("http://localhost:8080");
-        corsConfiguration.addAllowedMethod("GET");
-        corsConfiguration.addAllowedMethod("POST");
-        corsConfiguration.addAllowedMethod("PATCH");
-        corsConfiguration.addAllowedMethod("DELETE");
-        corsConfiguration.addAllowedMethod("OPTIONS");
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addExposedHeader("Authorization");
         corsConfiguration.addExposedHeader("userId");
