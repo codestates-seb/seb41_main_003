263ca048cc5b4240b15341f7e6b3e80bd615d98f
ansmeer008 <ansmeer008@gmail.com>
2023-01-19 10:08:52 +0900

    feat: ProfileContents 컴포넌트에 Pagination 추가 #103

--- client/src/components/profileSection/ProfileContents.jsx
+++ client/src/components/profileSection/ProfileContents.jsx
@@ -2,11 +2,11 @@ import PropTypes from 'prop-types';
 import TutorContents from './TutorContents';
 import TuteeContents from './TuteeContents';
 
-const ProfileContents = ({ user }) => {
+const ProfileContents = ({ user, pageInfo, setPage }) => {
   return (
     <div>
       {user.profileStatus === 'TUTOR' ? (
-        <TutorContents user={user} />
+        <TutorContents user={user} pageInfo={pageInfo} setPage={setPage} />
       ) : (
         <TuteeContents user={user} />
       )}
@@ -15,6 +15,8 @@ const ProfileContents = ({ user }) => {
 };
 ProfileContents.propTypes = {
   user: PropTypes.object,
+  pageInfo: PropTypes.object,
+  setPage: PropTypes.func,
 };
 
 export default ProfileContents;
