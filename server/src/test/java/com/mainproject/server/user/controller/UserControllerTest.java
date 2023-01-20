5ab9fb7bf6d1440f81ac6491f4e49104d6a611e3
sussa3007 <sussa1933@gmail.com>
2023-01-20 15:51:54 +0900

    test: User Controller Test 코드 작성 후 Docs 반영

--- server/src/test/java/com/mainproject/server/user/controller/UserControllerTest.java
+++ server/src/test/java/com/mainproject/server/user/controller/UserControllerTest.java
@@ -8,6 +8,8 @@ import com.mainproject.server.auth.redis.service.RefreshService;
 import com.mainproject.server.auth.token.JwtAuthorityUtils;
 import com.mainproject.server.auth.token.JwtTokenizer;
 import com.mainproject.server.config.SecurityConfig;
+import com.mainproject.server.dto.PageResponseDto;
+import com.mainproject.server.profile.dto.ProfileListResponseDto;
 import com.mainproject.server.profile.service.ProfileService;
 import com.mainproject.server.user.dto.UserPatchDto;
 import com.mainproject.server.user.dto.UserPostDto;
@@ -25,6 +27,10 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.headers.HeaderDocumentation;
@@ -38,10 +44,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
@@ -143,7 +150,6 @@ class UserControllerTest {
         // When
         RequestBuilder result = RestDocumentationRequestBuilders
                 .post("/users")
-                .header("Authorization", "Access Token Value")
                 .content(content)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
@@ -156,9 +162,6 @@ class UserControllerTest {
                         MockMvcRestDocumentation.document("postUser",
                                 ApiDocumentUtils.getRequestPreProcessor(),
                                 ApiDocumentUtils.getResponsePreProcessor(),
-                                HeaderDocumentation.requestHeaders(
-                                        headerWithName("Authorization").description("AccessToken")
-                                ),
                                 PayloadDocumentation.requestFields(
                                         List.of(
                                                 fieldWithPath("nickName").type(JsonFieldType.STRING).description("회원 닉네임"),
@@ -182,7 +185,7 @@ class UserControllerTest {
     }
 
     @Test
-    @DisplayName("회원 생성 TEST")
+    @DisplayName("회원 수정 TEST")
     @WithMockUser
     void patchUser() throws Exception {
         // Given
@@ -276,5 +279,130 @@ class UserControllerTest {
                                 )));
     }
 
+    @Test
+    @DisplayName("튜터 프로필 전체 리스트 조회 TEST")
+    @WithMockUser
+    void getTutors() throws Exception {
+        // Given
+        Map<String, String> params = new HashMap<>();
+        params.put("key", "TUTOR");
+        ProfileListResponseDto responseDto = ResponseStubData.createProfileListResponse();
+        List<ProfileListResponseDto> profileListResponse =
+                List.of(responseDto);
+        Page<ProfileListResponseDto> profileListResponseDto = new PageImpl<>(
+                profileListResponse,
+                PageRequest.of(0, 20),
+                1);
+
+        given(profileService.getTutorOrTuteeList(anyMap(), any(Pageable.class)))
+                .willReturn(PageResponseDto.of(profileListResponse, profileListResponseDto));
+        // When
+        RequestBuilder result = RestDocumentationRequestBuilders
+                .get("/users/tutors?page=0&sort=rate&search=test&subject=수학")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .characterEncoding(StandardCharsets.UTF_8.displayName());
+        // Then
+        mockMvc.perform(result)
+                .andExpect(status().isOk())
+                .andDo(MockMvcRestDocumentation.document("getSearchTutor",
+                        ApiDocumentUtils.getRequestPreProcessor(),
+                        ApiDocumentUtils.getResponsePreProcessor(),
+                        RequestDocumentation.requestParameters(
+                                parameterWithName("sort").description("정렬 조건 rate/updateAt"),
+                                parameterWithName("page").description("요청 페이지"),
+                                parameterWithName("search").description("검색 키워드(프로필 이름, 한줄소개 등 세부 정보 검색)"),
+                                parameterWithName("subject").description("튜터, 튜티의 과목 ex.'수학,과학'")
+                        ),
+                        PayloadDocumentation.responseFields(
+                                List.of(
+                                        fieldWithPath("data").type(JsonFieldType.ARRAY).description("결과 데이터"),
+                                        fieldWithPath("data[].profileId").type(JsonFieldType.NUMBER).description("프로필 식별자"),
+                                        fieldWithPath("data[].name").type(JsonFieldType.STRING).description("프로필 이름"),
+                                        fieldWithPath("data[].rate").type(JsonFieldType.NUMBER).description("평균 별점"),
+                                        fieldWithPath("data[].subjects").type(JsonFieldType.ARRAY).description("과목 리스트"),
+                                        fieldWithPath("data[].subjects[].subjectId").type(JsonFieldType.NUMBER).description("과목 식별자"),
+                                        fieldWithPath("data[].subjects[].subjectTitle").type(JsonFieldType.STRING).description("과목 명"),
+                                        fieldWithPath("data[].school").type(JsonFieldType.STRING).description("학교"),
+                                        fieldWithPath("data[].bio").type(JsonFieldType.STRING).description("한줄 소개"),
+                                        fieldWithPath("data[].profileImage").type(JsonFieldType.OBJECT).description("프로필 이미지"),
+                                        fieldWithPath("data[].profileImage.profileImageId").type(JsonFieldType.NUMBER).description("프로필 이미지 식별자"),
+                                        fieldWithPath("data[].profileImage.url").type(JsonFieldType.STRING).description("이미지 URL"),
+                                        fieldWithPath("data[].profileImage.createAt").type(JsonFieldType.STRING).description("프로필 이미지 생성 시각"),
+                                        fieldWithPath("data[].profileImage.updateAt").type(JsonFieldType.STRING).description("프로필 이미지 수정 시각"),
+                                        fieldWithPath("data[].createAt").type(JsonFieldType.STRING).description("프로필 생성 시각"),
+                                        fieldWithPath("data[].updateAt").type(JsonFieldType.STRING).description("프로필 최종 수정 시각"),
+                                        fieldWithPath("pageInfo").type(JsonFieldType.OBJECT).description("요청 페이지 정보"),
+                                        fieldWithPath("pageInfo.page").type(JsonFieldType.NUMBER).description("요청 페이지 - 0 = 1 페이지"),
+                                        fieldWithPath("pageInfo.size").type(JsonFieldType.NUMBER).description("페이지당 요청 회원"),
+                                        fieldWithPath("pageInfo.totalElements").type(JsonFieldType.NUMBER).description("총 멤버"),
+                                        fieldWithPath("pageInfo.totalPages").type(JsonFieldType.NUMBER).description("생성된 총 페이지")
+                                )
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("튜티 프로필 전체 리스트 조회 TEST")
+    @WithMockUser
+    void getTutees() throws Exception {
+        // Given
+        Map<String, String> params = new HashMap<>();
+        params.put("key", "TUTEE");
+        ProfileListResponseDto responseDto = ResponseStubData.createProfileListResponse();
+        List<ProfileListResponseDto> profileListResponse =
+                List.of(responseDto);
+        Page<ProfileListResponseDto> profileListResponseDto = new PageImpl<>(
+                profileListResponse,
+                PageRequest.of(0, 20),
+                1);
+
+        given(profileService.getTutorOrTuteeList(anyMap(), any(Pageable.class)))
+                .willReturn(PageResponseDto.of(profileListResponse, profileListResponseDto));
+        // When
+        RequestBuilder result = RestDocumentationRequestBuilders
+                .get("/users/tutees?page=0&sort=rate&search=test&subject=수학")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .characterEncoding(StandardCharsets.UTF_8.displayName());
+        // Then
+        mockMvc.perform(result)
+                .andExpect(status().isOk())
+                .andDo(MockMvcRestDocumentation.document("getSearchTutee",
+                        ApiDocumentUtils.getRequestPreProcessor(),
+                        ApiDocumentUtils.getResponsePreProcessor(),
+                        RequestDocumentation.requestParameters(
+                                parameterWithName("sort").description("정렬 조건 rate/updateAt"),
+                                parameterWithName("page").description("요청 페이지"),
+                                parameterWithName("search").description("검색 키워드(프로필 이름, 한줄소개 등 세부 정보 검색)"),
+                                parameterWithName("subject").description("튜터, 튜티의 과목 ex.'수학,과학'")
+                        ),
+                        PayloadDocumentation.responseFields(
+                                List.of(
+                                        fieldWithPath("data").type(JsonFieldType.ARRAY).description("결과 데이터"),
+                                        fieldWithPath("data[].profileId").type(JsonFieldType.NUMBER).description("프로필 식별자"),
+                                        fieldWithPath("data[].name").type(JsonFieldType.STRING).description("프로필 이름"),
+                                        fieldWithPath("data[].rate").type(JsonFieldType.NUMBER).description("평균 별점"),
+                                        fieldWithPath("data[].subjects").type(JsonFieldType.ARRAY).description("과목 리스트"),
+                                        fieldWithPath("data[].subjects[].subjectId").type(JsonFieldType.NUMBER).description("과목 식별자"),
+                                        fieldWithPath("data[].subjects[].subjectTitle").type(JsonFieldType.STRING).description("과목 명"),
+                                        fieldWithPath("data[].school").type(JsonFieldType.STRING).description("학교"),
+                                        fieldWithPath("data[].bio").type(JsonFieldType.STRING).description("한줄 소개"),
+                                        fieldWithPath("data[].profileImage").type(JsonFieldType.OBJECT).description("프로필 이미지"),
+                                        fieldWithPath("data[].profileImage.profileImageId").type(JsonFieldType.NUMBER).description("프로필 이미지 식별자"),
+                                        fieldWithPath("data[].profileImage.url").type(JsonFieldType.STRING).description("이미지 URL"),
+                                        fieldWithPath("data[].profileImage.createAt").type(JsonFieldType.STRING).description("프로필 이미지 생성 시각"),
+                                        fieldWithPath("data[].profileImage.updateAt").type(JsonFieldType.STRING).description("프로필 이미지 수정 시각"),
+                                        fieldWithPath("data[].createAt").type(JsonFieldType.STRING).description("프로필 생성 시각"),
+                                        fieldWithPath("data[].updateAt").type(JsonFieldType.STRING).description("프로필 최종 수정 시각"),
+                                        fieldWithPath("pageInfo").type(JsonFieldType.OBJECT).description("요청 페이지 정보"),
+                                        fieldWithPath("pageInfo.page").type(JsonFieldType.NUMBER).description("요청 페이지 - 0 = 1 페이지"),
+                                        fieldWithPath("pageInfo.size").type(JsonFieldType.NUMBER).description("페이지당 요청 회원"),
+                                        fieldWithPath("pageInfo.totalElements").type(JsonFieldType.NUMBER).description("총 멤버"),
+                                        fieldWithPath("pageInfo.totalPages").type(JsonFieldType.NUMBER).description("생성된 총 페이지")
+                                )
+                        )
+                ));
+    }
 
 }
