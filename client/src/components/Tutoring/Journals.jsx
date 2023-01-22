cb75c99ef92f9ba601d0f9a0bf5a02fbe9f1f850
ansmeer008 <ansmeer008@gmail.com>
2023-01-22 20:41:11 +0900

    feat: 과외 일지 리스트 Journals 컴포넌트로 분리 및 무한 스크롤 구현 #153

---
+++ client/src/components/Tutoring/Journals.jsx
@@ -0,0 +1,88 @@
+import PropType from 'prop-types';
+import { useRef, useState, useEffect } from 'react';
+import { HiSpeakerphone } from 'react-icons/hi';
+import styles from './JournalList.module.css';
+import { Link } from 'react-router-dom';
+import useScroll from '../../util/useScroll';
+import Profile from '../../recoil/profile';
+import axios from 'axios';
+import { useRecoilValue } from 'recoil';
+
+const Journals = ({
+  tutoring,
+  setTutoring,
+  pageInfo,
+  setPageInfo,
+  tutoringId,
+}) => {
+  const lastJournalRef = useRef(null);
+  const { profileId } = useRecoilValue(Profile);
+
+  const setIsNew = useScroll(() => {
+    if (pageInfo.page < pageInfo.totalPages - 1) {
+      scrollFunc(pageInfo.page + 1);
+    } else {
+      setIsNew(false);
+    }
+  }, lastJournalRef);
+
+  const scrollFunc = async (page) => {
+    await axios
+      .get(`/tutoring/details/${profileId}/${tutoringId}?page=${page}&size=6`)
+      .then(({ data }) => {
+        console.log(data.pageInfo);
+        setTutoring({
+          ...data.data,
+          dateNotices: [...tutoring.dateNotices, ...data.data.dateNotices],
+        });
+        console.log(tutoring.dateNotices);
+        setPageInfo(data.pageInfo);
+      })
+      .catch((err) => console.error(err.message));
+  };
+
+  return (
+    <ul className={styles.list} ref={lastJournalRef}>
+      {tutoring.dateNotices.map((el) => {
+        return (
+          <Link to={`/journal/${el.dateNoticeId}`} key={el.dateNoticeId}>
+            <li className={styles.li}>
+              <div className={styles.dateBox}>
+                <span className={styles.day}>
+                  {new Date(el.startTime).getDate()}
+                </span>
+                <span className={styles.yearMonth}>{`${new Date(
+                  el.startTime
+                ).getFullYear()}년 ${
+                  new Date(el.startTime).getMonth() + 1
+                }월`}</span>
+              </div>
+              <div className={styles.textBox}>
+                <span
+                  className={styles.goal}
+                >{`학습 목표 | ${el.dateNoticeTitle} `}</span>
+                <span
+                  className={styles.homework}
+                >{`과제 제출 완료 (${el.finishHomeworkCount}/${el.homeworkCount})`}</span>
+              </div>
+              <div className={styles.notiIcon}>
+                <HiSpeakerphone className={styles.hiSpeaker} />
+                공지
+              </div>
+            </li>
+          </Link>
+        );
+      })}
+    </ul>
+  );
+};
+
+Journals.propTypes = {
+  tutoring: PropType.object,
+  setTutoring: PropType.func,
+  pageInfo: PropType.object,
+  setPageInfo: PropType.func,
+  tutoringId: PropType.string,
+};
+
+export default Journals;
