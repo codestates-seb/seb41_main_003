4a5cc81d93b46ee0117cfa9afe0552960aa4c166
Seung gu Shin <less0805@gmail.com>
2023-01-22 22:22:35 +0900

    feat: 로딩 인디케이터 prop 추가 및 스타일 추가

--- client/src/components/LoadingIndicator.jsx
+++ client/src/components/LoadingIndicator.jsx
@@ -1,24 +1,40 @@
+import { forwardRef } from 'react';
 import styles from './LoadingIndicator.module.css';
+import PropType from 'prop-types';
 
-const LoadingIndicator = () => {
+const LoadingIndicator = forwardRef(({ isLoading, isSmall = false }, ref) => {
   return (
-    <div className={styles.contain}>
-      <div className={styles.loadingSpinner}>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+    <div
+      className={`${styles.contain} ${isSmall ? styles.small : ''}`}
+      ref={ref}
+    >
+      {isLoading && (
+        <div
+          className={`${styles.loadingSpinner} ${isSmall ? styles.small : ''}`}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      )}
     </div>
   );
+});
+
+LoadingIndicator.propTypes = {
+  isLoading: PropType.bool,
+  isSmall: PropType.bool,
 };
+
+LoadingIndicator.displayName = 'LoadingIndicator';
 
 export default LoadingIndicator;
