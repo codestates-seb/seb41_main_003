145ea27a9698de4feeb8941b96d5813b92cf3704
ansmeer008 <ansmeer008@gmail.com>
2023-01-23 13:34:08 +0900

    chore: size 파라미터, 주석 제거 #153

--- client/src/pages/Tutoring.jsx
+++ client/src/pages/Tutoring.jsx
@@ -7,11 +7,6 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 
-//TODO: 과외 관리 페이지는 하나이지만 종료된 과외 관리 & 진행중인 과외 관리 컴포넌트가 각각 만들어져 있음
-//종료된 과외를 관리하러 들어오느냐, 진행중인 과외를 관리하러 들어오느냐에 따라 분기해서 컴포넌트를 보여주어야 함 (현재는 더미 데이터 이용해 분기함)
-//tutoringStatus를 통해서 진행중인 과외와 종료된 과외를 구분할 수 있음 (진행 중 : UNCHECKED & PROGRESS, 종료 : FINISHED )
-//사용자가 튜터이냐 튜티이냐에 대한 분기는 (아마도) 로컬 스토리지에 저장될 user Status를 이용해 컴포넌트 안에서 합니다.
-
 const Tutoring = () => {
   const [tutoring, setTutoring] = useState({
     tutoringId: 0,
@@ -49,7 +44,7 @@ const Tutoring = () => {
 
   const getTutoringData = async () => {
     await axios
-      .get(`tutoring/details/${profileId}/${tutoringId}?size=6`)
+      .get(`tutoring/details/${profileId}/${tutoringId}`)
       .then(({ data }) => {
         setTutoring(data.data);
         setPageInfo(data.pageInfo);
