59eb33a98a972e68ab529e26e192a7c74daa5c50
sussa3007 <sussa1933@gmail.com>
2023-01-25 16:20:54 +0900

    be/refactor: OAuth2 Success Handler Social 회원 가입시 기존 회원 유효성 검증 및 예외 핸들링 추가

--- server/src/main/java/com/mainproject/server/auth/oauth2/OAuth2UserSuccessHandler.java
+++ server/src/main/java/com/mainproject/server/auth/oauth2/OAuth2UserSuccessHandler.java
@@ -48,10 +48,19 @@ public class OAuth2UserSuccessHandler extends SimpleUrlAuthenticationSuccessHand
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         String email = String.valueOf(oAuth2User.getAttributes().get("email"));
         String nickName = String.valueOf(oAuth2User.getAttributes().get("name"));
-        User user = saveUser(email, nickName);
+        try {
+            User user = saveUser(email, nickName);
+            String redirect = getRedirectUri(response, user);
+            getRedirectStrategy().sendRedirect(request, response, redirect);
+        } catch (ServiceLogicException se) {
+            if (se.getErrorCode().equals(ErrorCode.USER_EMAIL_EXISTS)) {
+                String exceptionRedirectUri = getExceptionRedirectUri(response, se.getErrorCode());
+                getRedirectStrategy().sendRedirect(request, response, exceptionRedirectUri);
+            }
+        }
 
-        String redirect = getRedirectUri(response, user);
-        getRedirectStrategy().sendRedirect(request, response, redirect);
+
+
     }
 
     private String getRedirectUri(HttpServletResponse response, User user) {
@@ -70,6 +79,17 @@ public class OAuth2UserSuccessHandler extends SimpleUrlAuthenticationSuccessHand
                 .toUriString();
     }
 
+    private String getExceptionRedirectUri(HttpServletResponse response, ErrorCode code) {
+        log.info("# OAuth2 Get ExceptionRedirectUri!");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.OK.value());
+        return UriComponentsBuilder.fromUriString("http://localhost:3000/auth?")
+                .queryParam("code", code.getStatus())
+                .queryParam("message", code.getMessage())
+                .build()
+                .toUriString();
+    }
+
     private User saveUser(String email, String nickName) {
         try {
             log.info(" #### OAuth2 Login ");
@@ -77,6 +97,7 @@ public class OAuth2UserSuccessHandler extends SimpleUrlAuthenticationSuccessHand
             if (user.getLoginType().equals(LoginType.SOCIAL)) {
                 return user;
             } else {
+                log.info(" #### OAuth2 User Email Exists");
                 throw new ServiceLogicException(ErrorCode.USER_EMAIL_EXISTS);
             }
         } catch (ServiceLogicException se) {
