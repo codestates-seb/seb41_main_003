655dc4603448cbe01a721a57fcd03cb8223e585e
ymymmz9 <ymymmz9@gmail.com>
2023-01-25 14:58:41 +0900

    fix: recoil 변수 effect제거

--- client/src/recoil/currentRoomId.js
+++ client/src/recoil/currentRoomId.js
@@ -1,27 +1,8 @@
 import { atom } from 'recoil';
 
-// key: 저장소에 저장되는 key 값
-// setSelf: 연결된 atom 의 값을 초기화 해주는 함수
-// onSet: 해당하는 atom 의 값이 변경이 되었을 때 실행되는 함수
-const localStorageEffect =
-  (key) =>
-  ({ setSelf, onSet }) => {
-    const savedValue = localStorage.getItem(key);
-    if (savedValue != null) {
-      setSelf(JSON.parse(savedValue));
-    }
-
-    onSet((newValue, _, isReset) => {
-      isReset
-        ? localStorage.removeItem(key)
-        : localStorage.setItem(key, JSON.stringify(newValue));
-    });
-  };
-
 const CurrentRoomIdState = atom({
   key: 'CurrentRoomId',
   default: 0,
-  effects: [localStorageEffect('current_room_id')],
 });
 
 export default CurrentRoomIdState;
