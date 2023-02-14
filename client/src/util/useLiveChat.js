f3b87f5ade8579f6d7a463518687f64418dbbd63
Seung gu Shin <less0805@gmail.com>
2023-02-14 16:40:14 +0900

    feat: 실시간 채팅을 위한 커스텀 훅 작성
    Co-authored-by: ansmeer008 <ansmeer008@users.noreply.github.com>
    Co-authored-by: 유영민 <ymymmz9@gmail.com>

---
+++ client/src/util/useLiveChat.js
@@ -0,0 +1,116 @@
+import { useState, useEffect, useRef } from 'react';
+import * as SockJS from 'sockjs-client';
+// eslint-disable-next-line import/no-unresolved
+import * as StompJs from '@stomp/stompjs';
+import axios from 'axios';
+import { useRecoilValue } from 'recoil';
+import CurrentRoomIdState from '../recoil/currentRoomId';
+import Profile from '../recoil/profile';
+
+const useLiveChat = () => {
+  const [text, setText] = useState('');
+  const [messageRoom, setMessageRoom] = useState([]);
+  const CurrentRoomId = useRecoilValue(CurrentRoomIdState);
+  const { profileId } = useRecoilValue(Profile);
+  const [receiverId, setReceiverId] = useState(0);
+  const client = useRef({});
+  const TOKEN = sessionStorage.getItem('authorization');
+
+  // ! 추후 URL API 사양에 맞게 변경 요망
+  const URL =
+    'http://ec2-15-165-186-53.ap-northeast-2.compute.amazonaws.com:8081/stomp/content';
+
+  useEffect(() => {
+    initialChatSetting();
+    connect();
+    return () => disconnect();
+  }, [CurrentRoomId]);
+
+  const initialChatSetting = async () => {
+    if (CurrentRoomId !== 0 && CurrentRoomId !== undefined)
+      await axios
+        .get(`/messages/rooms/${profileId}/${CurrentRoomId}`)
+        .then(({ data: { data } }) => {
+          if (profileId === data.tuteeId) {
+            setReceiverId(data.tutorId);
+          } else setReceiverId(data.tuteeId);
+          setMessageRoom(data);
+        })
+        .catch((err) => console.log(err));
+  };
+
+  const connect = () => {
+    client.current = new StompJs.Client({
+      webSocketFactory: () => new SockJS(URL),
+      connectHeaders: {
+        authorization: TOKEN,
+      },
+      debug: () => {},
+      reconnectDelay: 3000,
+      heartbeatIncoming: 2000,
+      heartbeatOutgoing: 2000,
+      onConnect: () => {
+        subscribe();
+      },
+      onStompError: (frame) => {
+        console.error(frame);
+      },
+    });
+
+    client.current.activate();
+  };
+
+  const disconnect = () => {
+    client.current.deactivate();
+  };
+
+  const subscribe = () => {
+    client.current.subscribe(`/sub/room/${CurrentRoomId}`, (res) => {
+      setMessageRoom((prev) => ({
+        ...prev,
+        messages: [...prev.messages, JSON.parse(res.body)],
+      }));
+    });
+  };
+
+  const publish = (type = '') => {
+    if (!client.current.connected) {
+      return;
+    }
+
+    let sendText = '';
+    switch (type) {
+      case '':
+        sendText = text;
+        break;
+      case 'matchingConfirm':
+        sendText = 'MAT_CHING_CON_FIRM';
+        break;
+      case 'matchingCancel':
+        sendText = 'MAT_CHING_CAN_CEL';
+        break;
+    }
+
+    client.current.publish({
+      destination: `/pub/chats/message/${CurrentRoomId}`,
+      body: JSON.stringify({
+        senderId: profileId,
+        receiverId: receiverId,
+        messageContent: sendText,
+        messageRoomId: CurrentRoomId,
+      }),
+    });
+    setText('');
+  };
+
+  return {
+    disconnect,
+    publish,
+    messageRoom,
+    setMessageRoom,
+    text,
+    setText,
+  };
+};
+
+export default useLiveChat;
